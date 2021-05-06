6fd248835d86f31a5970af24d4cf270d1089d402
Victoriasaurio <victoriasaurio.guzmanc@gmail.com>
2021-05-06 14:01:27 -0500

    User service

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@nestjs/common';
+//import { Connection, EntityManager } from 'typeorm';
 import { v4 as uuidv4 } from 'uuid';
 import { CreateUserDto } from './dto/create.user.dto';
 import { User } from './model/user.model'
@@ -7,10 +8,19 @@ const users: User[] = [];
 
 @Injectable() //Provider para hacer una injección de dependecias. En automático.
 export class UserService {
-    //Inserta en el arreglo un nuevo usuario
+    //constructor(private connection: Connection) {}
+
     public create(userDto: CreateUserDto): void {
+    //public async create(userDto: CreateUserDto): Promise<void> {
+        //const manager: EntityManager = this.connection.manager;
         users.push(new User(uuidv4(), userDto.name, userDto.username))
+        //await manager.save(User, new User(userDto.name, userDto.username))
     }
+
+    //Inserta en el arreglo un nuevo usuario
+    // public create(userDto: CreateUserDto): void {
+    //     users.push(new User(uuidv4(), userDto.name, userDto.username))
+    // }
 
     // Devuelve todos los User del Array
     public findAll(): User[] {
