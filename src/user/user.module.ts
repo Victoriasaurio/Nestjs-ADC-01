d0935158d4c65d6b80eb30e30998332e51416488
Victoriasaurio <victoriasaurio.guzmanc@gmail.com>
2021-05-06 14:01:04 -0500

    User module

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -1,10 +1,13 @@
 import { UserController } from './user.controller';
 import { UserService } from './user.service';
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { UserSchema } from './model/schema/user.schema';
 
 @Module({
-  imports: [],
+  imports: [TypeOrmModule.forFeature([UserSchema])],
   controllers: [UserController],
   providers: [UserService],
+  exports: [UserService]
 })
 export class UserModule {}
